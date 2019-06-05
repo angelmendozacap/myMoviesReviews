63d696873e98ecb13ade17042518be0f11247a1e
Alfredo Mendoza <kick-1930@hotmail.com>
2019-06-04 20:17:20 -0500

    chore: Update Configuration Example

--- app/config-example.php
+++ app/config-example.php
@@ -1,5 +1,15 @@
 <?php
 
+// DB variables
+define('DB_VARS', [
+    'DB_PORT'       => '3306',
+    'DB_SERVER'     => 'localhost',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => '',
+    'DB_NAME'       => 'my_database',
+]);
+
+// Cloudinary credentials
 \Cloudinary::config([
     "cloud_name" => "my_cloud_name",
     "api_key"    => "my_key",
